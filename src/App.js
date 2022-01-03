4debec1ea0603efe6275acfe76dde06184ead9a4
Anil K. Panta <anilpanta2053@gmail.com>
2022-01-03 10:00:23 +0545

    3rd first

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import "./App.css";
 import { useGetAllPostQuery } from "./services/post";
 
 function App() {
-  const { data, error, isLoading } = useGetAllPostQuery();
-  console.log(error, isLoading, data);
+  const data = useGetAllPostQuery();
+  console.log(data);
   // const [apiData, setApiData] = useState({});
-  return <div className="App">hi</div>;
+  return <div className="App">koooo</div>;
 }
 
 export default App;
