624141d633f55798165b0c53bc41bf2bf63f96a4
Anil K. Panta <anilpanta2053@gmail.com>
2022-01-03 14:20:26 +0545

    data mapped

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import "./App.css";
 import { useGetAllPostQuery } from "./services/post";
 
 function App() {
+  // const [apiData, setApiData] = useState({});
   const data = useGetAllPostQuery();
   console.log(data);
-  // const [apiData, setApiData] = useState({});
+  if (data.isLoading) return <div>Loading ....</div>;
+  if (data.isError) return <div>Unexpected error occured</div>;
+  if (data.data)
+    return (
+      <div>
+        {data.data.map((curElem) => {
+          return <h1>{curElem.title}</h1>;
+        })}
+      </div>
+    );
   return <div className="App">koooo</div>;
 }
 
